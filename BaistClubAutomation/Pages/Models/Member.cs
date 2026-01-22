f30be17f43af68ac881cb13cff3d658e5b52f4ea
sarumugam3 <sarumugam3@WA304-09>
2026-01-21 19:38:35 -0700

    Add project files.

---
+++ BaistClubAutomation/Pages/Models/Member.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaistClubAutomation.Pages.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; } 
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; } 
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; } 
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; } 
+
+        public string MembershipTier { get; set; } 
+
+        public bool IsShareholder { get; set; } 
+
+        public DateTime JoinDate { get; set; } 
+
+        public string Status { get; set; } = "Active"; 
+
+        [Column(TypeName = "decimal(4,1)")]
+        public decimal HandicapIndex { get; set; } 
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now; 
+    }
+}
+
---
+++ BaistClubAutomation/Pages/Models/ProspectiveMember.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaistClubAutomation.Pages.Models
+{
+    public class ProspectiveMember
+    {
+        [Key]
+        public int ApplicantId { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; } 
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; } 
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; } 
+
+        [Phone, StringLength(20)]
+        public string Phone { get; set; } 
+
+        [StringLength(200)]
+        public string Address { get; set; } 
+
+        public string DesiredTier { get; set; } 
+
+        public int SponsorId { get; set; } 
+
+        public DateTime ApplicationDate { get; set; } = DateTime.Now;
+
+        public string ApplicationStatus { get; set; } = "Pending"; 
+
+        public DateTime? CommitteeReviewDate { get; set; }
+
+        public string ApprovalNotes { get; set; } 
+    }
+}
